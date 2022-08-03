b2b20e93f7b6ce273cf037f0e49aa04da530b0c8
endijulian <endijulian080798@gmail.com>
2022-08-03 11:25:56 +0700

    campaign formatter json endpoint

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,4 +1,46 @@
 package campaign
 
 type CampaignFormatter struct {
+	ID               int    `json:"id"`
+	UserID           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageURL         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	formmaterCampaign := CampaignFormatter{
+		ID:               campaign.ID,
+		UserID:           campaign.UserID,
+		Name:             campaign.Name,
+		ShortDescription: campaign.ShortDescription,
+		ImageURL:         "",
+		GoalAmount:       campaign.GoalAmount,
+		CurrentAmount:    campaign.CurrentAmount,
+	}
+
+	if len(campaign.CampaignImages) > 0 {
+		formmaterCampaign.ImageURL = campaign.CampaignImages[0].FileName
+	}
+
+	return formmaterCampaign
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+
+	// if len(campaigns) == 0 {
+	// 	return []CampaignFormatter{}
+	// }
+
+	campaignsFormatter := []CampaignFormatter{}
+
+	for _, campaign := range campaigns {
+		CampaignFormatter := FormatCampaign(campaign)
+		campaignsFormatter = append(campaignsFormatter, CampaignFormatter)
+	}
+
+	return campaignsFormatter
+
 }
